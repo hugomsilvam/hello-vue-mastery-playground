dbe5ac88cb98e5403dd1d0ee26347d2577938e3c
Hugo Silva <hugomsilva@ua.pt>
2020-08-18 11:23:05 +0100

    feat(App.vue): add reactive property on vue component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>show me {{ product }} 🍌🍌🍌</h1>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
@@ -14,7 +15,15 @@ import HelloWorld from "./components/HelloWorld.vue";
     HelloWorld
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  // define a reactive property on a vue component
+  // https://michaelnthiessen.com/property-or-method-not-defined/
+  data() {
+    return {
+      product: "bananas"
+    };
+  }
+}
 </script>
 
 <style>
